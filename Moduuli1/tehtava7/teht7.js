a21281badfac55c0a848dfdf453865acc6db2011
rev2103 <roni.minkkinen@gmail.com>
2025-04-03 09:19:02 +0300

    Tehtävä 7 tehty

--- Moduuli1/tehtava7/teht7.js
+++ Moduuli1/tehtava7/teht7.js
@@ -1,7 +1,9 @@
 let sum = prompt('Give desired the number of dice rolls')
 let i = 0
+let sum = 0
 while (i<sum) {
     result = Math.floor(Math.random()*6)+1;
-    console.log(result)
+    sum += result
+    console.log(sum)
     i += 1
 }
