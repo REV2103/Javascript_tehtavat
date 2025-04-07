90a1f118ee72360b2041a1d845d6e10dd126fa64
ronim <roni.minkkinen@gmail.com>
2025-04-07 19:44:15 +0300

    Tehtävä 1 tehty

---
+++ moduuli3/t1/1.js
@@ -0,0 +1,9 @@
+'use strict'
+const div = document.querySelector('#target')
+const html =
+    `<div class = "my-list">
+    <li>First item</li>
+    <li>Second item</li>
+    <li>Third item</li>
+    </div>;`
+div.innerHTML = html;
